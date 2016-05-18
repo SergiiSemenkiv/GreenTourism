f6f90460d91f56a1dc792243c3e52934a196cca7
SergiiSemenkiv <ssv260689@gmail.com>
2016-05-18 13:40:00 +0300

    take points data from points.json in controller

--- client/controllers/points.js
+++ client/controllers/points.js
@@ -1,1 +1,7 @@
-
+app.controller('PointsList', ['$scope', '$https',
+  function($cope, $http){
+    $http.get('fixtures/points.json').success(function(data){
+      $scope.points = data;
+    });
+     
+}]);
