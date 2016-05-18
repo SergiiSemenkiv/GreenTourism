8e2d786e2c6f969eb4e208a36c9f3b94f8531f9a
SergiiSemenkiv <ssv260689@gmail.com>
2016-05-18 15:07:49 +0300

    Some small fix

--- client/controllers/points.js
+++ client/controllers/points.js
@@ -1,7 +1,10 @@
-app.controller('PointsList', ['$scope', '$https',
-  function($cope, $http){
+app.controller('PointsListCtrl', ['$scope', '$https',
+  function($scope, $http){
     $http.get('fixtures/points.json').success(function(data){
       $scope.points = data;
+      for(point in points){
+        marker = L.marker([v, h]).addTo(map);  
+      }
     });
      
 }]);
